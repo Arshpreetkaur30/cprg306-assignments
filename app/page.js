4dd810984505362c2cbfa6efbb75569679060420
Arshpreetkaur30 <123598186+Arshpreetkaur30@users.noreply.github.com>
2024-02-12 22:29:49 -0700

    page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import NewItem from './new-item'
-
-export default function Page() {
+import page from "./week-2/page"
+import Image from "next/image";
+import Link from "next/link";
+export default function Home() {
   return (
-    <div>
-      <NewItem />
-    </div>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="">
+        <h1> CPRG 306 : Web Development 2 - Assignments</h1>
+        <Link href="/week-2" >week-2</Link>
+        <Link href="/week-3" >week-3</Link>
+        <Link href="/week-4" >week-4</Link>
+      </div>
+    </main>
+  );
 }
