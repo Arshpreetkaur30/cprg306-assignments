2bb610c035b87ef6c634b479c4bd34617140c3be
Arshpreetkaur30 <123598186+Arshpreetkaur30@users.noreply.github.com>
2024-02-12 22:21:26 -0700

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
-import page from "./week-2/page"
-import Image from "next/image";
-import Link from "next/link";
-export default function Home() {
+import NewItem from './new-item'
+
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">
-        <h1> CPRG 306 : Web Development 2 - Assignments</h1>
-        <Link href="/week-2" >week-2</Link>
-      </div>
-    </main>
-  );
+    <div>
+      <NewItem />
+    </div>
+  )
 }
