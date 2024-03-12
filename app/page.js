ecb284d0a45587a3e95bc4e9ce72adbd5b24bcc3
Arshpreetkaur30 <123598186+Arshpreetkaur30@users.noreply.github.com>
2024-03-11 21:37:59 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,19 +6,18 @@ import page4 from "./week-2/page"
 import page5 from "./week-2/page"
 import Image from "next/image";
 import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="">
         <h1> CPRG 306 : Web Development 2 - Assignments</h1>
-    
         <Link href="/week-2" >week-2</Link>
         <Link href="/week-3" >week-3</Link>
         <Link href="/week-4" >week-4</Link>
         <Link href="/week-5" >week-5</Link>
         <Link href="/week-6" >week-6</Link>
-        <Link href="/weel-7" >week-7</Link>
-    
+        <Link href="/weel-7" >week-7</Link>    
       </div>
     </main>
   );
