5486f74b5bbab1e3e53251f5d163180c78791309
Arshpreetkaur30 <123598186+Arshpreetkaur30@users.noreply.github.com>
2024-03-11 22:28:26 -0600

    Update page.js

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -5,10 +5,10 @@ import ItemList from './item-list';
 
 const Page = () => {
   return (
-    <div className="bg-yellow-400 min-h-screen">
+    <div className="bg-blue-200 min-h-screen">
       {/*/ background changes yet to be made*/}  
     <div className="container mx-auto  ">
-      <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+      <h1 className="text-4xl font-semibold mb-6 border-b-2 border-gray-500">Shopping List</h1>
       <ItemList />
     </div>
      </div>
