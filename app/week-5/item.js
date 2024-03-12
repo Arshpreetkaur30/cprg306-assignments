4b9e5becf14a45d19f4daefa126b2f9b17b86fcf
Arshpreetkaur30 <123598186+Arshpreetkaur30@users.noreply.github.com>
2024-03-11 22:28:52 -0600

    Update item.js

--- app/week-5/item.js
+++ app/week-5/item.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 const Item = ({name, quantity, category}) => {
   return (
-    <ul className='border p-2 m-4 bg-purple-600 text-black'>
-        <li className='text-xl '  >Name: {name}</li>
+    <ul className='border border-2 p-2 m-4 bg-yellow-200 text-black'>
+        <li className='text-4xl font-bold'  >Name: {name}</li>
         <li className='text-sm ' >Buy {quantity} in {category}</li>
 {/* {console.log("Hello World")} */}
     </ul>
