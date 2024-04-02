d9a72d9f6e24e000c59409daf0e6d165b96f6b87
Arshpreetkaur30 <123598186+Arshpreetkaur30@users.noreply.github.com>
2024-04-02 12:20:37 -0600

    Add files via upload

---
+++ app/week-10/services/shopping-list-service.js
@@ -0,0 +1,24 @@
+import { db } from "./utils/firebase";
+import { collection, getDocs, addDoc, query } from "firebase/firestore";
+
+export async function getItems(userId) {
+  const items = [];
+  const q = query(collection(db, `users/${userId}/items`));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    items.push(doc.data());
+    console.log(doc.id, " => ", doc.data());
+  });
+
+  return items;
+}
+
+export async function addItem(userId, item) {
+  const docRef = await addDoc(collection(db, `users/${userId}/items`), {
+    id: item.id,
+    name: item.name,
+    category: item.category,
+    quantity: item.quantity,
+  });
+  console.log("Item is created with ID: ", docRef.id);
+}
---
+++ app/week-10/shopping-list/page.js
@@ -0,0 +1,37 @@
+"use client";
+
+import { useUserAuth } from "./utils/auth-context";
+import Link from 'next/link';
+
+export default function Page() {
+
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+    const handleSignIn = async () => await gitHubSignIn();
+
+    const handleSignOut = async () => await firebaseSignOut();
+
+    return (
+        <main>
+            <h1 className="flex justify-center items-center text-xl font-bold m-10 text-purple-300 h-full">
+             <div className="m-3">
+                 <p className="m-3">{user ? "Hi there!":"Please sign in:"}</p>
+                 <p className="m-3">{user?.email}</p>
+                 <p className="m-3">{user?.displayName}</p>
+             </div>
+            </h1>
+            <div className="flex justify-center">
+            {user ? (
+                <div>
+                    <button className="bg-blue-800 text-white font-semibold py-3 px-3 rounded m-3 p-2" onClick={handleSignOut}>Sign Out</button>
+                    <p className='text-black font-semibold hover:text-blue-500'>
+                        <Link href="/week-8/shopingList">Shopping List</Link>
+                    </p>
+                </div>
+                ) : (
+                <button className="bg-blue-800 text-white font-semibold py-3 px-3 rounded m-3 p-2" onClick={handleSignIn}>Sign In</button>
+            )}
+            </div>
+        </main>
+    );
+}
